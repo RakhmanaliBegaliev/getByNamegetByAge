d34fa25988f222d3033e47c1d8c3960d6b3cce15
Ins <begalieb.9797@gmil.com>
2022-11-30 18:39:56 +0300

    12rtgret

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,26 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Cat> cats = new ArrayList<>();
-        cats.add(new Cat("Murka",5));
-        cats.add(new Cat("Max",3));
-        cats.add(new Cat("Jony",5));
-
-            getAge(cats, 3);
-            getName(cats,"Murka");
+    ArrayList<Cat>cats = new ArrayList<>();
+    cats.add(new Cat("Murka",3));
+    cats.add(new Cat("Max",5));
+    cats.add(new Cat("Jone",2));
+    getByAge(cats, 5);
+    getByName(cats, "Jone");
     }
-    public static void getName(List<Cat>cats,String name){
-
-        for (int i = 0; i <cats.size() ; i++) {
-            if(cats.get(i).getName().equals(name)) {
+    public static void getByName(ArrayList<Cat>cats,String name){
+        for (int i = 0; i < cats.size(); i++) {
+            if(cats.get(i).getName().equals(name)){
                 System.out.println(cats.get(i));
             }
         }
     }
-    public static void getAge(List<Cat> cats, int age){
+    public static void getByAge(ArrayList<Cat>cats, int age){
         for (int i = 0; i < cats.size(); i++) {
             if(cats.get(i).getAge() == age){
                 System.out.println(cats.get(i));
             }
         }
     }
+    
 }
